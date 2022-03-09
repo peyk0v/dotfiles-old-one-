58270e2275737a78af3d63f6bda8aef2102b762e
peykov <peykovn@gmail.com>
2022-03-09 18:21:05 -0300

    gopls lsp

--- nvim/config/pkv.lua
+++ nvim/config/pkv.lua
@@ -87,7 +87,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'graphql', 'eslint' }
+local servers = { 'pyright', 'gopls', 'rust_analyzer', 'tsserver', 'graphql', 'eslint' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
